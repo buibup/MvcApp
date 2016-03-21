1b09a925df1408045c8615c464bae50204ab0bc2
buibup <b5209194@gmail.com>
2016-03-21 14:56:16 +0700

    Initial State

---
+++ MvcApp/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using MvcApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            var model = new AboutModel
+            {
+                Name = "Thai",Location="Bangkok, Thailand"
+            };
+
+            var listModel = new List<AboutModel>()
+            {
+                new AboutModel {Name="Eng", Location="London, English" },
+                new AboutModel {Name="USA", Location="USA" }
+            };
+
+            return View(listModel);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
